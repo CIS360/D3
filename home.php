960dae5fa4a9d7a870e19b0c2ccce77e862b85b4
ewanlp <ewanlp@gmail.com>
2018-11-11 11:17:59 -0700

    Update and rename home.html to home.php

--- home.php
+++ home.php
@@ -60,16 +60,46 @@ $(document).ready(function(){
     $('#lukeInfo').show(total);
   });
   $("#comment").click(function() {
-    $('#textComment').show();
-    $('#addComment').show();
+    $('#textComment').toggle();
+    $('#addComment').toggle();
   });
   $("#addComment").click(function() {
     $('#validComment').show();
     $('#textComment').addClass('is-valid');
+    $('#addComment').hide();
   });
   $("#likeBtn").click(function() {
     $('#likeGreen').show();
+    $('#unlike').show();
     $('#likeBtn').hide();
+    $('#comRem').hide();
+  });
+  $("#unlike").click(function() {
+    $('#likeGreen').toggle();
+    $('#unlike').toggle();
+    $('#likeBtn').toggle();
+    $('#comRem').toggle();
+  });
+  $("#comment2").click(function() {
+    $('#textComment2').toggle();
+    $('#addComment2').toggle();
+  });
+  $("#addComment2").click(function() {
+    $('#validComment2').show();
+    $('#textComment2').addClass('is-valid');
+    $('#addComment2').hide();
+  });
+  $("#likeBtn2").click(function() {
+    $('#likeGreen2').show();
+    $('#unlike2').show();
+    $('#likeBtn2').hide();
+    $('#comRem2').hide();
+  });
+  $("#unlike2").click(function() {
+    $('#likeGreen2').toggle();
+    $('#unlike2').toggle();
+    $('#likeBtn2').toggle();
+    $('#comRem2').toggle();
   });
 
 });
@@ -329,7 +359,32 @@ $(document).ready(function(){
 								<div row="">
 									<h2>Comments</h2>
 									<textarea class="form-control" id="exampleFormControlTextarea1" rows="3">I loved completing Some Green Action -  it made me feel like I was actually making a difference in the world even if it just a small action. The 9 points don't hurt, either!</textarea>
-								</div>
+                  <br />
+                  <div class="modal-footer">
+                    <div class="col-7 text-right">
+                      <div class="valid-feedback" id="likeGreen2" style="display:none;">
+                        Comment Liked!
+                      </div>
+                      <div class="invalid-feedback" id="comRem2" style="display:none;">
+                        Comment Unliked
+                      </div>
+                    </div>
+                    <div class="col-5 text-right">
+                      <button type="button" id="comment2" class="btn" style="color:white; background: #286739;">Comment</button>
+                      <button type="button" id="likeBtn2" class="btn" style="color:white; background: #286739;">Like</button>
+                      <button type="button" id="unlike2" class="btn" style="color:white; background: #286739; display:none;">Unlike</button>
+                    </div>
+
+                  </div>
+                  <div>
+                    <textarea class="form-control" id="textComment2" rows="3" style="display:none;" placeholder="Add Your Comment Here"></textarea>
+                    <div class="valid-feedback" id="validComment2" style="display:none;">
+                      Comment addded!
+                    </div>
+                  </div>
+                  <br />
+                  <button type="button" id="addComment2" class="btn btn-block" style="display:none; color:white; background: #286739;">Comment on Post</button>
+                </div>
 				      </div>
 				    </div>
 				  </div>
@@ -375,11 +430,20 @@ $(document).ready(function(){
                   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Wow, this website is so fun and easy to use. What a great application! Bhaa.</textarea>
                   <br />
                   <div class="modal-footer">
-                    <button type="button" id="likeBtn" class="btn" style="color:white; background: #286739;">Like</button>
-                    <div class="valid-feedback" id="likeGreen" style="display:none;">
-                      Comment Liked!
+                    <div class="col-7 text-right">
+                      <div class="valid-feedback" id="likeGreen" style="display:none;">
+                        Comment Liked!
+                      </div>
+                      <div class="invalid-feedback" id="comRem" style="display:none;">
+                        Comment Unliked
+                      </div>
                     </div>
-                    <button type="button" id="comment" class="btn" style="color:white; background: #286739;">Comment</button>
+                    <div class="col-5 text-right">
+                      <button type="button" id="comment" class="btn" style="color:white; background: #286739;">Comment</button>
+                      <button type="button" id="likeBtn" class="btn" style="color:white; background: #286739;">Like</button>
+                      <button type="button" id="unlike" class="btn" style="color:white; background: #286739; display:none;">Unlike</button>
+                    </div>
+
                   </div>
                   <div>
                     <textarea class="form-control" id="textComment" rows="3" style="display:none;" placeholder="Add Your Comment Here"></textarea>
